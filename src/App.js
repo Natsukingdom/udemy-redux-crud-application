3873eb3c73ffb5d5a022cf6b3c75f76f124c0524
Natsukingdom <natsuki.m.1992@gmail.com>
2018-09-08 22:04:24 +0900

    create class components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 // jsxを利用する際には，Reactの定義が必要
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-      <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("hoge")}} />
-      </React.Fragment>
-    )
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("hoge")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
 
-  }
+const App = () => {
+  return (
+    <div>
+      Hi!Im Funtion
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow</div>
 }
 
 export default App;
