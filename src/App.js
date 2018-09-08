85c0ec60791da727357a3fb7d77b87f240380bdc
Natsukingdom <natsuki.m.1992@gmail.com>
2018-09-08 22:33:55 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 // jsxを利用する際には，Reactの定義が必要
 import React from 'react';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//       <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={() => {console.log("hoge")}} />
-//       </React.Fragment>
-//     )
-//   }
-// }
-
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "Noname" }
+  ]
   return (
     <div>
-      Hi!Im Funtion
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, i) => {
+          return <User name={profile.name} age={profile.age} key={i} />
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow</div>
+const User = (props) => {
+  return <div>Hello, My name is {props.name}, and {props.age} years old !!</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
