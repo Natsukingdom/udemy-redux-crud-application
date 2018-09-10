93cdd5a680f7198e176fe257dfbe910641cd9528
Natsukingdom <natsuki.m.1992@gmail.com>
2018-09-10 13:57:21 +0900

    implement reducer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import './index.css';
+import reducer from './reducers'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 // アプリケーション内に存在するすべてのreducersを統括するためのファイル
 // 結合用のパッケージをリダックスパッケージからimportして利用する
 
-import { combimineReducers } from 'redux';
+import { combineReducers } from 'redux';
 import count from './count';
 
 export default combineReducers({ count })
