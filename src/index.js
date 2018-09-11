1bbda67e10bbb4077ca5a305df63661063bcb290
Natsukingdom <natsuki.m.1992@gmail.com>
2018-09-11 15:59:37 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-
 import './index.css';
 import reducer from './reducers'
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer)
+
+// 以下のようにして app を provider で lap して provider の store 属性に store を渡すと
+// storeがapp内で利用できるようになる．
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
